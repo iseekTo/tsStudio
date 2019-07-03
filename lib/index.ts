05683f8c1b23c815ec81ba8e07dbdbfd7072d523
liyubo <liyubo@gaodun.com>
2019-07-03 14:59:36 +0800

    增加tslint

--- lib/index.ts
+++ lib/index.ts
@@ -1,16 +1,15 @@
-
 type places = string[] | string;
 
-interface book {
-    price?: number
-    label?: places
-    where?: places
+interface IBook {
+    price?: number;
+    label?: places;
+    where?: places;
 }
 
-let book: book = {
+let book: IBook = {
     price: 200,
-    label: '封神',
-    where: '天河以南'
-}
+    label: "封神",
+    where: "天河以南"
+};
 
-console.log(book, '111') 
+console.log(book, "111");
