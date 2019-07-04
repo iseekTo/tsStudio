4214f3950ba1424e0c9916869bf6a5aab1d17dfd
liyubo <liyubo@gaodun.com>
2019-07-04 14:51:01 +0800

    promise

---
+++ lib/question/promise.ts
@@ -0,0 +1,19 @@
+interface IResponse<R> {
+    message: string,
+    result: R,
+    code: number
+}
+
+/**
+ * 异步传输数据时 我们并不知道返回的数据是什么类型 
+ * 此时定义一个接口容器来接收传过来的参数。
+ */
+async function resp(): Promise<IResponse<any>> {
+    return {
+        message: '异步信息',
+        result: ['1', 2, false],
+        code: 200
+    }
+}
+
+export let tryData = async () => await resp()
--- lib/question/returnfn.ts
+++ lib/question/returnfn.ts
@@ -5,11 +5,3 @@
 export function returnFn(f: Function, ...args: string[]) {
     return f(args)
 }
-
-export let outNames = (arr: string[]) => {
-    arr.push('1,', 'true', '{}', 'eeeeee')
-    let s = arr
-    return s
-}
-
-returnFn(outNames)
