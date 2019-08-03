7cbe871b07113d8a67fdd752d2d6be2f178c03ad
alias <1214046231@qq.com>
2019-08-03 18:26:40 +0800

    update: input common method of onChange

---
+++ lib/common/changeInput.ts
@@ -0,0 +1,11 @@
+/**
+ * if add input onChange event
+ * you can do this 
+ * if env is React
+ * We can distinguish different values according to the name attribute of input.
+ */
+let handleOnchange = (val: string, event: React.HandlerChange<HTMLInputElement>) => {
+  let composeInput: object = {};
+  composeInput[event.target.name] = val
+  this.setState(composeInput)
+}
