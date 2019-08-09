2c345eed2997b4593262320cb56bd1b948b079a1
liyubo <liyubo@gaodun.com>
2019-08-09 15:09:13 +0800

    范式定义类型

--- lib/question/for.ts
+++ lib/question/for.ts
@@ -1,5 +1,19 @@
+/**
+ * 快速设置键的统一类型
+ */
+
 export type size = 'small' | 'large' | 'middle';
 
 export type vals = {
-    [k in size]: any
+    [k in size]: string     // 方式一
+}
+
+// 方式二
+type Records<Q extends keyof any, T> = { [P in Q]: T }
+
+type bs = Records<size, boolean>    
+
+let is_boolean: bs = {
+    small: '1',      // error 
+    large: true      // success
 }
