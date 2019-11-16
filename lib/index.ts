e4214556e5d2868d109baac3ebd756fdf1070f6c
janow <15036830368@163.com>
2019-11-16 17:47:42 +0800

    微改

--- lib/index.ts
+++ lib/index.ts
@@ -2,20 +2,18 @@ import { generErrorObj, reveiveResult } from './question/error'
 import { tryData } from './question/promise'
 console.log('--------------------------------------------')
 
-
-
-
-
-
-
-
-
-
-
-
-
-
 tryData().then(res => console.log('da2222222ta：', res))
+
+function strlog<S extends string | null>( 
+    text?: S 
+): S extends string ? string : null {
+    if (!text) return null
+    if (typeof text == 'string') return text.toUpperCase()
+}
+
+console.log(strlog());
+
+
 
 // let gener = generErrorObj();
 // console.log("push之后的结果：", gener);
