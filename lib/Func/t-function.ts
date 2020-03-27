b5fa311b47bc9b87fcf1634b57545b8bba22eae5
iseekto <1214046231@qq.com>
2020-03-27 11:40:57 +0800

    auto commit shell

--- lib/Func/t-function.ts
+++ lib/Func/t-function.ts
@@ -34,4 +34,17 @@ let only_func: PickOnlyFuncType<Connected> = {
     }
 }
 
+type someso = {
+    school: school,
+    n: number,
+    r: RegExp
+}
 
+type str = P<someso, 'school'>
+
+// P泛型接收一个 T 类型，内部 U 从类型 T 获取全部的 键 --- 前置条件
+// 定义 K 为键 遍历之前的 U(可以理解为一个string[])
+// 最后根据第二个选择的 类型键 渲染对应的数据类型
+type P<T, U extends keyof T> = {
+    [K in U]: T[K]
+} 
