095c739a23e3b342eedf43eae73f5170d2a5d5d0
iseekTo <1214046231@qq.com>
2020-03-28 22:29:37 +0800

    auto commit shell

--- noun/condition.ts
+++ noun/condition.ts
@@ -2,12 +2,13 @@
  * @Date: 2020-03-27 16:07:43
  * @Author: lyb
  * @LastEditors: lyb
- * @LastEditTime: 2020-03-27 16:25:51
+ * @LastEditTime: 2020-03-28 22:27:37
  */
 
 type animal = 'dog' | 'cat' | 'lion' | 'elephant';
 
 type filter<S> = S extends 'cat' ? S : never;
 
+// filter 'cat' type.
 type cat = filter<animal>
 
