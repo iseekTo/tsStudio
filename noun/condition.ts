5db7398cd21e5886cb3c208a980e311ead330662
iseekTo <1214046231@qq.com>
2020-04-02 14:58:43 +0800

    auto commit shell

--- noun/condition.ts
+++ noun/condition.ts
@@ -2,7 +2,7 @@
  * @Date: 2020-03-27 16:07:43
  * @Author: lyb
  * @LastEditors: lyb
- * @LastEditTime: 2020-03-28 22:27:37
+ * @LastEditTime: 2020-03-31 11:05:45
  */
 
 type animal = 'dog' | 'cat' | 'lion' | 'elephant';
