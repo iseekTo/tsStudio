719bfc2c266a2eafac500d9af4648708a0ba65d6
iseekTo <1214046231@qq.com>
2020-04-04 21:58:56 +0800

    auto commit shell

--- commit.sh
+++ commit.sh
@@ -1,7 +1,13 @@
 #!/bin/sh
+###
+# @Date: 2020-03-27 11:46:15
+# @Author: lyb
+# @LastEditors: lyb
+# @LastEditTime: 2020-04-04 21:58:17
+###
 
 git add -A
 
 git commit -m 'auto commit shell'
 
-git push origin master -f
+git push origin -u master -f
