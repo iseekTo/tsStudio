fa5d4bd77be26312c4c6d457e332df47999656af
iseekTo <1214046231@qq.com>
2020-04-05 18:49:12 +0800

    auto commit shell

--- tests/string.test.ts
+++ tests/string.test.ts
@@ -1,12 +1,28 @@
+/*
+ * @Date: 2020-03-27 13:24:18
+ * @Author: lyb
+ * @LastEditors: lyb
+ * @LastEditTime: 2020-04-05 18:48:58
+ */
 import { it, describe } from 'mocha'
 import { expect } from 'chai'
-import { isString } from '../lib/util/util'
+import { isString, isRegExp } from '../util/util'
 
 describe('util.ts -> type', () => {
     describe('isString', () => {
         it('typeof a value should be "string" ', () => {
             const str: boolean = isString('hahahah')
             expect(str).to.equal(true)
+        })
+    })
+})
+
+
+describe('util.ts -> reg', () => {
+    describe('isRegExp', () => {
+        it('typeof the value is "regExp', () => {
+            const reg: unknown = isRegExp(/\s+/g)
+            expect(reg).to.equal(true)
         })
     })
 })
--- util/util.ts
+++ util/util.ts
@@ -2,7 +2,7 @@
  * @Date: 2019-11-08 16:42:28
  * @Author: lyb
  * @LastEditors: lyb
- * @LastEditTime: 2020-03-29 20:25:04
+ * @LastEditTime: 2020-04-05 18:38:22
  */
 const obj = Object.prototype;
 
@@ -39,36 +39,3 @@ export const isNumber = (nur: unknown): nur is number =>
 export const isRegExp = (reg: unknown): reg is RegExp => {
     return '[object RegExp]' === obj.toString.call(reg);
 };
-
-const findFirst = <L>(list: Array<L>, f: Function): cacheType => {
-    return list.filter(f)[0];
-}
-
-
-type cacheType = {
-    original: object;
-    copy: unknown;
-}
-
-
-export const deepCopy = (obj: unknown): unknown => {
-    let copy: unknown = {};
-    if (obj === null || typeof obj !== 'object') {
-        return obj
-    }
-
-    const siuteType = obj?.constructor.name;
-
-    if (siuteType === 'Array') {
-        copy = []
-    }
-
-    if (siuteType === 'Object') {
-        copy = {}
-    }
-
-    for (const k in obj) {
-        copy[k] = deepCopy(obj[k])
-    }
-    return copy;
-} 
