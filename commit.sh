4081cb0d47768325e73a7b8e965e410ef188caef
iseekTo <1214046231@qq.com>
2020-04-05 18:19:47 +0800

    auto commit shell

--- commit.sh
+++ commit.sh
@@ -2,8 +2,8 @@
 ###
 # @Date: 2020-03-27 11:46:15
 # @Author: lyb
-# @LastEditors: lyb
-# @LastEditTime: 2020-04-04 21:58:17
+ # @LastEditors: lyb
+ # @LastEditTime: 2020-04-05 18:18:24
 ###
 
 git add -A
--- index.d.ts
+++ index.d.ts
@@ -1,9 +1,16 @@
+/*
+ * @Date: 2020-03-27 11:46:15
+ * @Author: lyb
+ * @LastEditors: lyb
+ * @LastEditTime: 2020-04-05 18:19:38
+ */
 // reference 代表普通文件里的 import 关键字
+// eslint-disable-next-line @typescript-eslint/triple-slash-reference
 /// <reference path="base.d.ts" />
 
 declare const DEV: string;
 
-declare interface IPos extends IOpt {
+declare interface Ipos extends IOpt {
     some: string;
 }
 
@@ -11,7 +18,7 @@ declare interface IPos extends IOpt {
 // ## 找不到第三方模块时，有两种方法可以消除ts的报警
 
 // 1、在加载第三方模块内部声明
-declare const yourModuleName: any;
+declare const yourModuleName: unknown;
 
 // 2、在根目录新建 modules.d.ts
 declare module 'yourModuleName' {
