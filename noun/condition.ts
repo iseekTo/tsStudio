63e068e9d3813bf0479192803c62a97081c5d054
iseekTo <1214046231@qq.com>
2020-06-16 18:12:40 +0800

    auto commit shell

--- noun/condition.ts
+++ noun/condition.ts
@@ -2,7 +2,7 @@
  * @Date: 2020-03-27 16:07:43
  * @Author: lyb
  * @LastEditors: lyb
- * @LastEditTime: 2020-05-12 17:48:44
+ * @LastEditTime: 2020-06-16 18:12:15
  */
 
 export type animal = 'dog' | 'cat' | 'lion' | 'elephant';
@@ -12,5 +12,3 @@ export type os = 'android' | 'ios'
 // filter 'cat' type.
 export type cat = filter<animal>
 export type filter<S> = S extends 'cat' ? S : never;
-
-
